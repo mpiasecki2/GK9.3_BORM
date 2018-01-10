9edd6b4ec123b92b9b84a56e4edc259c84d2182e
mpiasecki <mpiasecki@student.tgm.ac.at>
2018-01-10 09:37:30 +0100

    commiting persistency changes

---
+++ src/main/java/com/hellokoding/auth/WebApplication.java
@@ -0,0 +1,19 @@
+package com.hellokoding.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
